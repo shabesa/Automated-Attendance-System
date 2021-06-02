5c70873474e2b4582f222bd9739d5d30791ecc16
k.a.shabesa <54354603+shabesa@users.noreply.github.com>
2021-06-02 18:51:20 +0530

    basic ui and json

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import json
+import tkinter
+from tkinter import messagebox
+from tkinter.constants import BOTTOM, CENTER, LEFT
+
+def checkJSON():
+    fileRead = open("settings.txt", "r", encoding="utf-8")
+    settings = json.load(fileRead)
+    fileRead.close()
+
+    first_use = settings['checks']['first']
+    #print(first_use)
+
+    if first_use == True:
+        UI(first_use)
+        settings['checks']['first'] = False
+        file = open('settings.txt', 'w')
+        json.dump(settings, file)
+        file.close()
+    else:
+        UI(first_use)
+  
+
+def UI(method):
+
+        body = tkinter.Tk()
+        labelHeading = tkinter.Label(master=body, text="Automated Attendance System", width=30)
+        startButton = tkinter.Button(master=body, text="Start", width=15)
+        stopButton = tkinter.Button(master=body, text="Stop", width=15)
+
+        labelHeading.pack(side=tkinter.TOP)
+        startButton.pack(side=tkinter.LEFT)
+        stopButton.pack(side=tkinter.LEFT)
+        body.mainloop()
+
+
+if __name__=="__main__":
+    checkJSON()

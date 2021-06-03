5993de6dd7474b5a029f3ae95e148bab567c14e0
k.a.shabesa <54354603+shabesa@users.noreply.github.com>
2021-06-03 16:41:33 +0530

    added recognition code

--- face_recog.py
+++ face_recog.py
@@ -3,13 +3,37 @@ import os
 import cv2
 import numpy
 
-class Recog:
-    def __init__(self):
-        path = 'images'
-        images = []
-        classNames = []
-        myList = os.listdir(path)
-        print(myList)
+path = 'images'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    currentImg = cv2.imread(f'{path}/{cl}')
+    images.append(currentImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodedList = []
+    for image in images:
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        encoded = face_recognition.face_encodings(image)[0]
+        encodedList.append(encoded)
+    return encodedList
+
+encodedList = findEncodings(images)
+print(len(encodedList))
 
 
-int1 = Recog()
+# faceLoc = face_recognition.face_locations(imgRDJ)[0]
+# encodeRDJ = face_recognition.face_encodings(imgRDJ)[0]
+# cv2.rectangle(imgRDJ, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (0, 0, 255), 2)
+
+# faceLocTest = face_recognition.face_locations(imgTest)[0]
+# encodeTest = face_recognition.face_encodings(imgTest)[0]
+# cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (0, 0, 255), 2)
+
+# result = face_recognition.compare_faces([encodeRDJ], encodeTest)
+# faceDis = face_recognition.face_distance([encodeRDJ], encodeTest)
+# print(result, faceDis)

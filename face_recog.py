672f8fd53d8e4b4f6766ab0c5c1f190104b8af49
k.a.shabesa <54354603+shabesa@users.noreply.github.com>
2021-06-04 09:31:19 +0530

    complete face recognition code

--- face_recog.py
+++ face_recog.py
@@ -2,6 +2,7 @@ import face_recognition
 import os
 import cv2
 import numpy as np
+from datetime import datetime
 
 path = 'images'
 images = []
@@ -22,6 +23,18 @@ def findEncodings(images):
         encodedList.append(encoded)
     return encodedList
 
+def markAttendance(name):
+    with open('attendance.csv', 'r+') as file:
+        myDataList = file.readlines()
+        nameList=[]
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            file.writelines(f'\n{name},{dtString}')
+
 encodedList = findEncodings(images)
 print('Encoding Complete')
 
@@ -29,8 +42,8 @@ cap = cv2.VideoCapture(0)
 
 while True:
     frame, img = cap.read()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    #imgS = cv2.resize(img, (0, 0), None, 0.5, 0.5)
+    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     faceLocInFrame = face_recognition.face_locations(imgS)
     encodeCurrentFrame = face_recognition.face_encodings(imgS, faceLocInFrame)
@@ -44,10 +57,11 @@ while True:
             name = classNames[matchIndex].upper()
             print(name)
             y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            #y1, x2, y2, x1 = y1*2, x2*2, y2*2, x1*2
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
             cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 0, 255), cv2.FILLED)
             cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1 , (255, 255, 255), 2)
+            markAttendance(name)
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)

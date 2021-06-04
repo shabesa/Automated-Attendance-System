804bda537132bbaf9e10d50bd4ea886832a163f1
k.a.shabesa <54354603+shabesa@users.noreply.github.com>
2021-06-04 15:31:17 +0530

    major code changes

---
+++ UI.py
@@ -0,0 +1,51 @@
+import tkinter
+import pyttsx3
+import json
+from tkinter import *
+from tkinter.constants import BOTTOM
+from audio import AudioEngine
+
+audioEngine = AudioEngine(pyttsx3.init('sapi5'), 0)
+
+def IntroUI():
+    body1 = tkinter.Tk()
+
+    introLabel = tkinter.Label(master=body1, text="""Welcome to Automated Attendance System. Use start to activate the system. Click ok to continue.""", width=75)
+    okButton = tkinter.Button(master=body1, text="OK", width=15, command=body1.destroy)
+    introLabel.pack()
+    okButton.pack(side=BOTTOM)
+    body1.mainloop()
+
+
+
+def MainUI():
+
+    fileRead = open("settings.txt", "r", encoding="utf-8")
+    settings = json.load(fileRead)
+    fileRead.close()
+
+    grade = settings['class']
+    section = settings['section']
+    
+    def startButtonFunc():
+        audioEngine.speak("Starting camera")
+        startButton['state'] = DISABLED
+        stopButton['state'] = NORMAL
+        
+    def stopButtonFunc():
+        audioEngine.speak("Stopping camera")
+        startButton['state'] = NORMAL
+        stopButton['state'] = DISABLED
+    
+    body2 = tkinter.Tk()
+
+    labelHeading = tkinter.Label(master=body2, text="Automated Attendance System", width=30)
+    labelClass = tkinter.Label(master=body2,  text=f'{grade}-{section}')
+    startButton = tkinter.Button(master=body2, text="Start", width=15, command=startButtonFunc, state=NORMAL)
+    stopButton = tkinter.Button(master=body2, text="Stop", width=15, command=stopButtonFunc, state=DISABLED)
+
+    labelHeading.pack(side=tkinter.TOP)
+    labelClass.pack()
+    startButton.pack(side=tkinter.LEFT)
+    stopButton.pack(side=tkinter.LEFT)
+    body2.mainloop()
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import os
 import json
-import face_recognition
-import cv2
 import pyttsx3
-import tkinter
-from tkinter import *
-from tkinter.constants import BOTTOM
 from audio import AudioEngine
+from UI import MainUI, IntroUI
 
 audioEngine = AudioEngine(pyttsx3.init('sapi5'), 0)
 
@@ -26,28 +22,6 @@ def checkJSON():
         os.system('python "E:\\shabesa\\Projects\\Shabesa\\Automated-Attendance-System\\main.py"')
     if first_use == False:
         MainUI()
-
-def IntroUI():
-    body1 = tkinter.Tk()
-
-    introLabel = tkinter.Label(master=body1, text="""Welcome to Automated Attendance System. Use start to activate the system. Click ok to continue.""", width=75)
-    okButton = tkinter.Button(master=body1, text="OK", width=15, command=body1.destroy)
-    introLabel.pack()
-    okButton.pack(side=BOTTOM)
-    body1.mainloop()
-
-def MainUI():
-
-        body2 = tkinter.Tk()
-        labelHeading = tkinter.Label(master=body2, text="Automated Attendance System", width=30)
-        startButton = tkinter.Button(master=body2, text="Start", width=15)
-        stopButton = tkinter.Button(master=body2, text="Stop", width=15)
-
-        labelHeading.pack(side=tkinter.TOP)
-        startButton.pack(side=tkinter.LEFT)
-        stopButton.pack(side=tkinter.LEFT)
-        body2.mainloop()
-
 
 if __name__=="__main__":
     checkJSON()

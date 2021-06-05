d5f3dd7296a780e2c01103329768e1d566b7385c
k.a.shabesa <54354603+shabesa@users.noreply.github.com>
2021-06-05 19:29:24 +0530

    UI window resolution

--- UI.py
+++ UI.py
@@ -19,6 +19,7 @@ class UI:
     #Provides intro of the project
     def IntroUI(self):
         body1 = tkinter.Tk()
+        body1.title("Welcome")
 
         introLabel = tkinter.Label(master=body1, text="""Welcome to Automated Attendance System. Use start to activate the system. Click ok to continue.""", width=75)
         okButton = tkinter.Button(master=body1, text="OK", width=15, command=body1.destroy)
@@ -57,6 +58,8 @@ class UI:
 
         
         body2 = tkinter.Tk()
+        body2.geometry("910x100")
+        body2.title('Automated Attendance System')
 
         labelHeading = tkinter.Label(master=body2, text="Automated Attendance System", width=30)
         labelClass = tkinter.Label(master=body2,  text=f'{grade}-{section}')

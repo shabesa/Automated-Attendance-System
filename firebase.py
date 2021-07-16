79c8c98884e442dd5df772fb697b31f07b384cae
k.a.shabesa <54354603+shabesa@users.noreply.github.com>
2021-07-16 17:10:23 +0530

    added UpdateBase class

---
+++ firebase.py
@@ -0,0 +1,45 @@
+import firebase_admin
+from firebase_admin import auth
+from firebase_admin import credentials
+from firebase_admin import firestore
+import json
+
+class UpdateBase:
+    
+    def __init__(self):
+        cred = credentials.Certificate('firebase-sdk.json')
+        firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
+
+        fileRead = open("settings.txt", "r", encoding="utf-8")
+        settings = json.load(fileRead)
+        fileRead.close()
+        self.email = settings['auth']['user']
+        self.password = settings['auth']['password']
+
+    def createuser(self):
+        user = auth.create_user(email = self.email, password = self.password)
+        print(f"User created successfully : {user.uid}")
+
+    def addData(self, date, name, time, verification):
+        doc_ref = self.db.collection(date).document()
+        doc_ref.set({
+            u'Name': name,
+            u'Time': time,
+            u'Verification': verification
+        })
+
+    def readData(self, date):
+        users_ref = self.db.collection(date)
+        docs = users_ref.stream()
+
+        for doc in docs:
+            documents = doc.to_dict()
+            print(documents)
+        return documents
+
+# UpdateBase().readData('date')
+# UpdateBase().addData('16-07-2021', 'Kishore', '05:10:05', 1)
+
+
+
